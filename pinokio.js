934da5ad9fdc2e9d8693ecf4b42d535220fd78da
cocktailpeanut <cocktailpeanuts@proton.me>
2023-07-13 10:35:49 -0400

    init

---
+++ pinokio.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "start": "start.json",
+  "menu": [{
+    "html": "<i class='fa-solid fa-microchip'></i> Install",
+    "href": "install.json"
+  }, {
+    "html": "Terminal",
+    "href": "start.json"
+  }]
+}
