0faa540107efc26296b74fc02628c6ea5c5a4f6a
cocktailpeanut <cocktailpeanuts@proton.me>
2023-07-20 17:40:19 -0400

    meta

--- pinokio.js
+++ pinokio.js
@@ -1,4 +1,7 @@
 module.exports = {
+  "title": "Audio Webui",
+  "description": "A webui for different audio related Neural Networks",
+  "icon": "icon.png",
   "start": "start.json",
   "menu": [{
     "html": "<i class='fa-solid fa-microchip'></i> Install",
